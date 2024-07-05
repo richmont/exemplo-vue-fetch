288bb7c1b19244c589ca3fa1292553c4b35b139f
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2024-07-05 15:19:33 -0300

    Botão de atualizar para aplicar configurações

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import UserNationality from './ControlPanel/UserNationality.vue'
+import UserApplySetupButton from './ControlPanel/UserApplySetupButton.vue'
 </script>
 <template>
   <div class="control-panel-container">
     <UserNationality />
+    <UserApplySetupButton />
   </div>
 </template>
 <style></style>
---
+++ src/components/ControlPanel/UserApplySetupButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import storeUsersSetup from '../../stores/storeUsersSetup.ts'
+import storeJSONData from '../../stores/storeJSONData.ts'
+const userSetup = storeUsersSetup()
+const JSONData = storeJSONData()
+async function refreshData() {
+  userSetup.buildURL()
+
+  await JSONData.getData()
+}
+</script>
+<template><button @click="refreshData">Atualizar</button></template>
+<style></style>
